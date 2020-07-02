86c2bcb5ae3ded04c40d227e609816faf80d0d24
Piscinao <henhvj@gmail.com>
2020-07-02 19:59:19 -0300

    Create reactions with a loop

--- script.js
+++ script.js
@@ -27,7 +27,30 @@ function createSnake(){
   }
 }
 
+// Event Listener chama a update conforme a tecla apertada e passa como argumento o evento de tecla que
+// setado na função 
+document.addEventListener('keydown', update);
+
+
+function update(event){
+  // Muda apenas se a direção setada não for ao contrário da direção atual
+  if(event.keyCode == 37 && direction != "right") direction = "left";
+  if(event.keyCode == 38 && direction != "down") direction = "up";
+  if(event.keyCode == 39 && direction != "left") direction = "right";
+  if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
 function startGame(){
+
+  // plano cartesiano vai até de 16 para um lado 0 de outro aumenta e diminui
+  // se passar do plano seta a posição para 0
+  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+ 
   createBG();
   createSnake();
 
