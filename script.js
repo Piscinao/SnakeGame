404c60092019e39baf9f76064cc9109bfc83fff2
Piscinao <henhvj@gmail.com>
2020-07-02 19:20:17 -0300

    Create function bg and snake

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
 
-function criarBG(){
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+
+}
+
+function createBG(){
   // Define a cor
   context.fillStyle = "lightgreen";
   // Desenha o retangulo 4 parametros posição x e y 
@@ -10,4 +17,12 @@ function criarBG(){
 
 }
 
-criarBG();
+function createSnake(){
+  for(i=0; i < snake.length; i++){
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
+createBG();
+createSnake();
